5855fa4889d2c78e66930875fc86c80e6bb35fd5
Sahil799717 <22914029sahil@viva-technology.org>
2025-06-04 13:37:24 +0530

    productcontroller check kar

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,7 @@
+import { v2 as cloudinary } from "cloudinary";
+
+
+
 //function for add product
 
 const addProduct = async (req, res) => {
@@ -12,10 +16,25 @@ const addProduct = async (req, res) => {
       bestseller,
     } = req.body;
 
-    const image1 = req.files.image1[0];
-    const image2 = req.files.image2[0];
-    const image3 = req.files.image3[0];
-    const image4 = req.files.image4[0];
+    const image1 = req.files.images1 && req.files.image1[0];
+    const image2 = req.files.images2 && req.files.image2[0];
+    const image3 = req.files.images3 && req.files.image3[0];
+    const image4 = req.files.images4 && req.files.image4[0];
+
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item !== undefined
+    );
+
+    let imagesUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image"
+        });
+        return result.secure_url;
+      })
+    )
+
+
 
     console.log(
       name,
@@ -26,7 +45,7 @@ const addProduct = async (req, res) => {
       sizes,
       bestseller
     );
-    console.log(image1, image2, image3, image4);
+    console.log(imagesUrl);
 
     res.json({});
   } catch (error) {
